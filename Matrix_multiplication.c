a74e0da9819b3ef0d6c882852fe2a3f350a3ff22
Onkar-25 <55799837+Onkar-25@users.noreply.github.com>
2020-06-19 18:07:45 +0530

    Update Matrix_multiplication.c

--- Matrix_multiplication.c
+++ Matrix_multiplication.c
@@ -1,16 +1,17 @@
 #include<stdio.h>
 main()
 {
-    int mat1[20][20],mat2[20][20],ans[20][20],r1,c1,r2,c2,i,j,k,sum;
+    int mat1[20][20],mat2[20][20],r1,c1,r2,c2,k;
+    int ans[20][20];
     printf("Enter the rows and columns of first matrix=");
     scanf("%d %d",&r1,&c1);
     printf("\nEnter the rows and columns of second matrix=");
     scanf("%d %d",&r2,&c2);
     if(c1==r2)
     {
-    for(i=0;i<r1;i++)
+    for(int i=0;i<r1;i++)
     {
-        for(j=0;j<c1;j++)
+        for(int j=0;j<c1;j++)
         {
             mat1[i][j]=rand()%100;
             mat2[i][j]=rand()%100;
@@ -18,9 +19,9 @@ main()
     }
     printf("\nElements of first matrix are\n");
     printf("mat1=   ");
-    for(i=0;i<r1;i++)
+    for(int i=0;i<r1;i++)
     {
-        for(j=0;j<c1;j++)
+        for(int j=0;j<c1;j++)
         {
             printf("%d\t",mat1[i][j]);
         }
@@ -29,21 +30,23 @@ main()
 
     printf("\nElements of second matrix are\n");
     printf("mat2=   ");
-    for(i=0;i<r2;i++)
+    for(int i=0;i<r2;i++)
     {
-        for(j=0;j<c2;j++)
+        for(int j=0;j<c2;j++)
         {
             printf("%d\t",mat2[i][j]);
         }
         printf("\n\t");
     }
     printf("\nmat1+mat2 =\t");
-    for(i=0;i<r1;i++)
+    for(int i=0;i<r1;i++)
     {
-        for(j=0;j<c2;j++)
+        
+        for(int j=0;j<c2;j++)
         {
-            sum=0;
-            for(k=0;k<c1;k++)
+            
+            int sum=0;
+            for(int k=0;k<c1;k++)
             {
                 sum=sum+(mat1[i][k]*mat2[k][j]);
             }
